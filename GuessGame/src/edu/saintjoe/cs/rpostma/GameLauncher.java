36a10549591c25c25443fd18e71cbd53ab60f5c2
rpostma <rpostma@localhost>
2016-02-12 16:06:46 -0500

    added code from book, not tested yet.

---
+++ GuessGame/src/edu/saintjoe/cs/rpostma/GameLauncher.java
@@ -0,0 +1,8 @@
+package edu.saintjoe.cs.rpostma;
+
+public class GameLauncher {
+	public static void main (String[] args) {
+		GuessGame game = new GuessGame();
+		game.startGame();
+	}
+}
---
+++ GuessGame/src/edu/saintjoe/cs/rpostma/Player.java
@@ -0,0 +1,11 @@
+package edu.saintjoe.cs.rpostma;
+
+public class Player {
+	int number = 0;
+
+	public void guess() {
+		number = (int) (Math.random() * 10);
+		System.out.println("I'm guessing " + number);
+	}
+
+}
